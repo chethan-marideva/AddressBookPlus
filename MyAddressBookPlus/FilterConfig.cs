7fa0677e7ea4bd9011316c56e3d83f406cb01baa
Chethan M <chethan.marideva@gmail.com>
2020-02-22 20:41:48 +0530

    App Insigts

---
+++ MyAddressBookPlus/FilterConfig.cs
@@ -0,0 +1,11 @@
+using System.Web.Mvc;
+
+namespace MyAddressBook_{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
+        }
+    }
+}
